d12b15ab928f34b09c993018538a896f1ceb4103
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2022-08-24 16:07:50 -0400

    Remove results output

--- modules/capa/output.tf
+++
@@ -1,3 +0,0 @@
-output "install_results" {
-  value = ssh_resource.install_capa.result
-}
--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
 output "capa_node_ip" {
   value = aws_instance.capa_server.public_ip
 }
-
-output "result" {
-  value = module.capa.install_results
-}
