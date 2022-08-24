ec1780e103a5c75aeba4a5c1548b54e705acab99
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2022-08-24 15:55:35 -0400

    Add clusterctl init support in CAPA module

--- modules/capa/capa.tf
+++ modules/capa/capa.tf
@@ -9,9 +9,22 @@ resource "ssh_resource" "install_capa" {
     "sudo mv ./clusterctl /usr/local/bin/clusterctl",
     "curl -L https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/${var.capa_version}/clusterawsadm-linux-amd64 -o clusterawsadm",
     "chmod +x clusterawsadm",
-    "sudo mv clusterawsadm /usr/local/bin"
+    "sudo mv clusterawsadm /usr/local/bin",
+    "mkdir -p .aws",
+    "echo -e '[default] \nregion = ${var.aws_region} \noutput = json' > .aws/config",
+    "echo -e '[default] \naws_access_key_id = ${var.aws_access_key} \naws_secret_access_key = ${var.aws_secret_key}' > .aws/credentials",
+    "sudo cp /etc/rancher/k3s/k3s.yaml /tmp/k.yaml",
+    "sudo chown ${var.node_username}:${var.node_username} /tmp/k.yaml"
   ]
-  commands    = ["clusterawsadm version"]
+
+  commands = [
+    "clusterawsadm bootstrap iam create-cloudformation-stack",
+    <<EOT
+    export AWS_B64ENCODED_CREDENTIALS=$(clusterawsadm bootstrap credentials encode-as-profile)
+    clusterctl init --infrastructure aws --kubeconfig=/tmp/k.yaml
+    EOT
+  ]
+
   user        = var.node_username
   private_key = var.ssh_private_key_pem
 }
