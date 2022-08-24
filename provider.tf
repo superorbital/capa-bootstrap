0498f0a484ecba2e730187a9d6c74c48bd631b30
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2022-08-24 15:58:46 -0400

    Upgrade TLS and AWS providers

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.18.0"
+      version = "~> 4.27"
     }
     local = {
       source  = "hashicorp/local"
@@ -10,7 +10,7 @@ terraform {
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "3.4.0"
+      version = "~> 4.0"
     }
   }
   required_version = ">= 1.0.0"
