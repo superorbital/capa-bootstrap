dbfd06ce6eb05bd9ed99d4d54c182d1322b74e11
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2023-12-08 15:02:46 -0500

    Upgrade TF providers

--- modules/capa/provider.tf
+++ modules/capa/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     local = {
       source  = "hashicorp/local"
-      version = "2.2.3"
+      version = "~> 2.4"
     }
     ssh = {
       source  = "loafoe/ssh"
-      version = "2.1.0"
+      version = "~> 2.4"
     }
   }
   required_version = ">= 1.0.0"
--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.27"
+      version = "~> 5.30"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.2.3"
+      version = "~> 2.4"
     }
     tls = {
       source  = "hashicorp/tls"
