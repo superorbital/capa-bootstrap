fc9879bf3e56b291082e2e20625f300de29ee593
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2023-12-28 13:53:36 -0500

    Add description to `capa_node_ip` output

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "capa_node_ip" {
-  value = aws_instance.capa_server.public_ip
+  value       = aws_instance.capa_server.public_ip
+  description = "Public IP address of the CAPA management node. Useful for SSH purposes."
 }
